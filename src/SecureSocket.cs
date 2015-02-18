6ec2e9c95fb6f322f7650767b39a403966a0a3c9
Andre Soares <andregsilv@gmail.com>
2015-02-18 10:27:10 -0200

    Removed unused references

--- src/SecureSocket.cs
+++ src/SecureSocket.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Sockets;
 using System.Net;
-using System.Runtime;
-using System.Collections;
 using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
+using System.Net.Sockets;
 using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
-using System.Threading;
 
 namespace SecureSockets
 {
